a8beb817c86cfd7310c07d8fcc02900fc0cdaf6b
mmizanrhn <mizanrhn1@gmail.com>
2018-04-04 20:08:31 +0600

    change array file

--- array.php
+++ array.php
@@ -1,31 +1,15 @@
-    <?php
-       // $months=array('january','fab','march','april','may','june');
-       // var_dump($months);
-       // print_r($months);
-       //echo $months[3][1];
-       //$users=array(
-       //'name'=>array('Mizan','Shakil','Kamal'),'Nizam',
-       //'id'=>'123',
-       // 'age'=>'34'
-       // );
-       //echo $users['name'];
-       // for($i=0;$i<10;$i++){
-       // echo $i;
-       // }
-       //foreach($users as $user =>$i){
-       // echo "<li>".$user."=". $i."</li>";
+<!--  PHP 5 Multidimensional Arrays  -->
+<?php
+$cars = array(
+  array("BMW","22","17"),
+  array("TOYOTA","12","27"),
+  array("PREMIO","18","15"),
+  array("LEXUS","12","17")
+  );
 
-       $sites=array(
+  echo $cars[0][0]." Stocks ".$cars[0][1]." Sold ".$cars[0][2]."<br>";
+  echo $cars[1][0]." Stocks ".$cars[1][1]." Sold ".$cars[1][2]."<br>";
+  echo $cars[2][0]." Stocks ".$cars[2][1]." Sold ".$cars[2][2]."<br>";
+  echo $cars[3][0]." Stocks ".$cars[3][1]." Sold ".$cars[3][2];
 
-            'google'=> 'https://www.google.com',
-            'youtube'=> 'https://www.youtube.com',
-            'facebook'=> 'https://www.facebook.com',
-            'yahoo'=> 'https://www.yahoo.com'
-        );
-
-            foreach($sites as $site_name =>$site_url){
-
-               echo "<li><a href='$site_url'>$site_name</a></li>";
-            }
-
-    ?>
+?>
